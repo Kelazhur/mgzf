35da67a53f98c8b85371df8675f6747e84d4689f
flagsa <flagsa@qq.com>
2019-01-15 16:57:50 +0800

    添加敏感字符替换系统

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 char arr[1024];
 int index=0;
+int isMingan(char x){
+    int count=0;
+    for(int i=0;i<index;i++)
+    {
+        if(x==arr[i])
+        {
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 int main()
 {
     printf("进入敏感字符管理系统\n");
@@ -66,6 +84,26 @@ int main()
         }
         if(code==4)
         {
+            printf("请输入一段英文：\n");
+            char neirong[1024];
+            scanf("%s",neirong);
+            for(int i=0;1;i++)
+            {
+                if(neirong[i]=='\0')
+                {
+                    break;
+                }
+                if(isMingan(arr[i])==1)
+                {
+                    neirong[i]='*';
+                }
+                printf("%c",neirong[i]);
+            }
+            printf("\n");
+            printf("替换完成，点击回车继续\n");
+            char enter;
+            scanf("%c",&enter);
+            scanf("%c",&enter);
         }
         if(code==5)
         {
